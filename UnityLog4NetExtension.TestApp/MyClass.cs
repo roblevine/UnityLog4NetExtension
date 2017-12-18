42f80e58a2535cddf1bdd3539d0945fc8da9caa8
Rob Levine <rob@roblevine.co.uk>
2017-12-18 13:51:37 +0000

    Add test console app

---
+++ UnityLog4NetExtension.TestApp/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using log4net;
+
+namespace UnityLog4NetExtension.TestApp
+{
+    public class MyClass
+    {
+        private readonly ILog _logger;
+
+        public MyClass(ILog logger)
+        {
+            _logger = logger;
+        }
+
+        public void Test()
+        {
+            _logger.Info("HELLO WORLD!");
+        }
+    }
+}
---
+++ UnityLog4NetExtension.TestApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Config;
+using Unity;
+using UnityLog4NetExtension.Log4Net;
+
+namespace UnityLog4NetExtension.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                XmlConfigurator.Configure();
+                var container = new UnityContainer();
+                container.RegisterType<MyClass>();
+                container.AddNewExtension<Log4NetExtension>();
+
+                var myClass = container.Resolve<MyClass>();
+                myClass.Test();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                if (Debugger.IsAttached)
+                {
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
